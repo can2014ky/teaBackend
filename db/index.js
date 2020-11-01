04881b7df745b218f1fab54740d2aefdceaf966a
zhoucan1 <zhoucan1@360.cn>
2020-11-01 19:05:05 +0800

    feat: 修改MySQL数据连接配置信息

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ const mysql      = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '123456',
-  database : 'tea'
+  password : 'root',
+  database : 'miniprogram'
 });
 
 connection.connect();
