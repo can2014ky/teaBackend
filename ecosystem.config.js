fb8e51c3da213954850d96fd7ad30e0c162ba305
zhoucan1 <zhoucan1@360.cn>
2020-11-04 18:53:36 +0800

    feat: [search] pm2部署

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps : [
+    {
+      name: 'tea',
+      script: './bin/www',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy : {
+    production : {
+      user : 'root',
+      host : '47.104.148.45',
+      ref  : 'origin/master',
+      repo : 'git@github.com:can2014ky/teaBackend.git',
+      path : '/project/tea',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
